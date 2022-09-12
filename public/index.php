c3ff388a0caa56ac0b241a1230cdc4e27b86a11d
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-12 20:15:02 +0430

    new

--- public/index.php
+++ public/index.php
@@ -16,12 +16,12 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-/*if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
     require __DIR__.'/../storage/framework/maintenance.php';
-}*/
-if (file_exists(__DIR__.'/local/storage/framework/maintenance.php')) {
-    require __DIR__.'/local/storage/framework/maintenance.php';
 }
+/*if (file_exists(__DIR__.'/local/storage/framework/maintenance.php')) {
+    require __DIR__.'/local/storage/framework/maintenance.php';
+}*/
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -33,8 +33,8 @@ if (file_exists(__DIR__.'/local/storage/framework/maintenance.php')) {
 |
 */
 
-//require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/local/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
+//require __DIR__.'/local/vendor/autoload.php';
 
 
 /*
@@ -48,9 +48,9 @@ require __DIR__.'/local/vendor/autoload.php';
 |
 */
 
-//$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app = require_once __DIR__.'/local/bootstrap/app.php';
+//$app = require_once __DIR__.'/local/bootstrap/app.php';
 $app->bind('path.public', function() {
     return __DIR__;
 });
