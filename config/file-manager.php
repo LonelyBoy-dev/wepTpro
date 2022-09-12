23b44aa294e03d21f827b6a9c64a82960831f965
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-12 20:29:39 +0430

    new

--- config/file-manager.php
+++ config/file-manager.php
@@ -25,7 +25,7 @@ return [
      * List of disk names that you want to use
      * (from config/filesystems)
      */
-    'diskList' => ['uploads'],
+    'diskList' => ['uploads','images'],
 
     /**
      * Default disk for left manager
