a4b5c09faca51e89d6fa85e6bbcf614092b1253d
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-12 20:16:42 +0430

    new

---
+++ bootstrap/cache/special_comments_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\SpecialComments\\Providers\\SpecialCommentsServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\SpecialComments\\Providers\\SpecialCommentsServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/work_sample_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\WorkSample\\Providers\\WorkSampleServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\WorkSample\\Providers\\WorkSampleServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
