6c44d78defb19b4288af1b853a52563a7cb6fff5
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-13 10:17:54 +0430

    new commands

--- app/Http/Controllers/Creepy/PortalSite.php
+++ app/Http/Controllers/Creepy/PortalSite.php
@@ -20,7 +20,7 @@ class PortalSite extends Controller
         $crawler->filter('.elementor-widget-container .elementor-post.elementor-grid-item')->each(function ($node) {
             $title = $node->filter('.elementor-post__title a')->text();
             $slug = $this->make_slug($title);
-            sleep(1);
+            sleep(5);
             $image=$node->filter('.elementor-post__thumbnail__link img')->attr("src");
             $post = Post::where('slug', $slug)->first();
             $link =  $node->filter('.elementor-post__thumbnail__link')->attr("href");
