7cd286a234abf275f74ecdd75006cd29f02781b9
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-13 17:56:04 +0430

    new commands

--- themes/myWebsite/src/resources/views/contact.blade.php
+++ themes/myWebsite/src/resources/views/contact.blade.php
@@ -127,8 +127,8 @@
                                             <div class="mb-3">
                                                 <label class="form-label">شماره موبایل </label>
                                                 <div class="form-icon position-relative">
-                                                    <i data-feather="book" class="fea icon-sm icons"></i>
-                                                    <input name="mobile" id="subject" class="form-control ps-5" placeholder="شماره موبایل  :" value="{{old('mobile')}}">
+                                                    <i class="fa-regular fa-mobile"></i>
+                                                    <input name="mobile" id="mobile" class="form-control ps-5" placeholder="شماره موبایل  :" value="{{old('mobile')}}">
                                                 </div>
                                             </div>
                                         </div><!--end col-->
