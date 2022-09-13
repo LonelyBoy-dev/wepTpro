d9e9c227b312d5acf495a45343183726aaca7bc7
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-13 10:57:51 +0430

    new commands

--- app/Http/Controllers/Creepy/PortalSite.php
+++ app/Http/Controllers/Creepy/PortalSite.php
@@ -17,10 +17,10 @@ class PortalSite extends Controller
     {
         $client = new Client();
         $crawler = $client->request('GET', 'https://www.kiantc.com/articles');
-        $crawler->filter('.align-items-stretch .earticle-col-item')->each(function ($node) {
+        $crawler->filter('.justify-content-center .align-items-stretch')->each(function ($node) {
             $title = $node->filter('.card-title.font-weight-bold a')->text();
             $slug = $this->make_slug($title);
-            $image=$node->filter('.align-items-stretch .earticle-col-item .img-fluid')->attr("src");
+            $image=$node->filter('.justify-content-center .align-items-stretch .img-fluid')->attr("src");
             $post = Post::where('slug', $slug)->first();
             $link =  $node->filter('.card-title.font-weight-bold a')->attr("href");
             dd('title= '.$title.'   slug= '.$slug.'   image= '.$image.'   link= '.$link);
