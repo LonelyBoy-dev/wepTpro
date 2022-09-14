00c2b0ef9ea9664aef1f6aa5564c7f09b3dffe3b
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-14 08:12:53 +0430

    new commands

--- public/index.php
+++ public/index.php
@@ -51,9 +51,9 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 //$app = require_once __DIR__.'/local/bootstrap/app.php';
-$app->bind('path.public', function() {
+/*$app->bind('path.public', function() {
     return __DIR__;
-});
+});*/
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
