2d906b95ff9ace47d23bc6502b7573a989f25513
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-14 10:17:27 +0430

    new commands

---
+++ bootstrap/cache/features_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Features\\Providers\\FeaturesServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Features\\Providers\\FeaturesServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
