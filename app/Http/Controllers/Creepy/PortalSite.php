ad6a219bd1190e968717013d20e53cd3d1f95a85
Mahdi <mahdi77yousefi2015@gmail.com>
2022-09-15 14:06:28 +0430

    new commands

--- app/Http/Controllers/Creepy/PortalSite.php
+++ app/Http/Controllers/Creepy/PortalSite.php
@@ -29,6 +29,8 @@ class PortalSite extends Controller
                 $inside_post = $client->request('GET', $link);
                 $inside_post->each(function ($item) {
                     $meta_description=$item->filterXpath('//meta[@name="description"]')->attr('content');
+                    $meta_description=utf8_encode($meta_description);
+                    dd($meta_description);
                     $title = $item->filter('h1')->text();
                     $content = $item->filter('.col-md-9.col-12.pr-lg-4.pl-lg-0.d-xl-0 .text-right')->html();
                     $image = session('Tamneel-image');
