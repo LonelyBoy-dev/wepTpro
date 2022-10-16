1c4953c8666ede9398789ff64b87c172c57f39f2
Mahdi <mahdi77yousefi2015@gmail.com>
2022-10-16 16:42:43 +0330

    new commands

--- themes/myWebsite/src/resources/views/widgets/work-samples.blade.php
+++ themes/myWebsite/src/resources/views/widgets/work-samples.blade.php
@@ -25,7 +25,7 @@
                                     <h6 class="text-light tag mb-0 text-right"><a href="{{ route('front.workSample.show', ['work_sample' => $item]) }}" class="text-white title ">{{$item->title}}</a></h6>
                                 </div>
                                 <div class="client text-right">
-                                    @if($item->applicant)<small class="text-light user d-block"><i class="uil uil-user"></i>{{$item->applicant}}</small>@endif
+                                    @if($item->applicant)<small class="text-light user d-block"><i class="uil uil-user"></i> {{$item->applicant}}</small>@endif
                                     <small class="text-light date"><i class="muil uil-calendar-alt"></i>
                                         @if($item->publish_date)
                                             {{Verta::instance($item->publish_date)->format(' %d %B %Y')}}
