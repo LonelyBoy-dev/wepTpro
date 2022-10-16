4d2c61734faa854623ee7bde2fdc3f1d3c49de96
Mahdi <mahdi77yousefi2015@gmail.com>
2022-10-16 16:59:29 +0330

    new commands

--- themes/myWebsite/src/resources/views/widgets/work-samples.blade.php
+++ themes/myWebsite/src/resources/views/widgets/work-samples.blade.php
@@ -19,8 +19,7 @@
                     <div class="col-lg-4 col-md-6 mt-4 pt-2">
                         <div class="card border-0 work-container work-modern position-relative d-block overflow-hidden rounded post-shadow">
                             <div class="portfolio-box-img position-relative overflow-hidden text-center">
-                                <iframe src="{{$item->webSite}}" class="item-container img-fluid mx-auto"  style="height: 292px"></iframe>
-{{--                                <img class="item-container img-fluid mx-auto" src="{{asset($item->image)}}" alt="{{$item->title}}" style="height: 292px"/>--}}
+                                <img class="item-container img-fluid mx-auto" src="{{asset($item->image)}}" alt="{{$item->title}}" style="height: 292px"/>
                                 <div class="overlay-work bg-dark"></div>
                                 <div class="content">
                                     <h6 class="text-light tag mb-0 text-right"><a href="{{ route('front.workSample.show', ['work_sample' => $item]) }}" class="text-white title ">{{$item->title}}</a></h6>
